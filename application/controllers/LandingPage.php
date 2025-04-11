ebb21138ff10677e8dd161a5a7b6ea1e5fe843ff
bcp-nadeem <nadeem.qureshi@bimcap.com>
2025-04-11 19:08:39 +0800

    Update

--- application/controllers/LandingPage.php
+++ application/controllers/LandingPage.php
@@ -1,9 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class LandingPage extends CI_Controller {
-    function __construct() {
-        parent::__construct();
-    }
     public function index() {
         $this->load->view('users/index');
     }
